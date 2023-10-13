11127022e0ac2c6f9ce7c815ca71457868c400cd
AEG-LaptopThinkpadP16s <52181546+adrianegraphene@users.noreply.github.com>
2023-10-12 20:44:19 -0700

    First setup

---
+++ src/components/footer.js
@@ -0,0 +1,65 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <p>
+          info@fyncom.com | We are located in Irvine, CA, focused in the USA &
+          operate globally.
+        </p>
+      </div>
+      <ul className="footer-links">
+        <li>
+          <a href="https://www.fyncom.com">FynCom Website</a>
+        </li>
+        <li>
+          <a href="mailto:info@fyncom.com">info@fyncom.com</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/company/fyncom">LinkedIn</a>
+        </li>
+        <li>
+          <a href="https://twitter.com/Fyn_Com">Twitter</a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/FynCom/">Facebook</a>
+        </li>
+        <li>
+          <a href="https://www.fyncom.com/use-cases">Product</a>
+        </li>
+        <li>
+          <a href="https://www.fyncom.com/use-cases">Reward Campaigns</a>
+        </li>
+        <li>
+          <a href="https://www.fyncom.com/about">About</a>
+        </li>
+        <li>
+          <a href="https://www.fyncom.com/pricing">Pricing</a>
+        </li>
+        {/*<li>*/}
+        {/*  <a href="https://www.fyncom.com/partners">Partners</a>*/}
+        {/*</li>*/}
+        <li>
+          <a href="https://www.fyncom.com/help-center">Help</a>
+        </li>
+        <li>
+          <a href="https://www.fyncom.com/blog">Blog</a>
+        </li>
+        <li>
+          <a href="https://www.fyncom.com/contact">Contact Us</a>
+        </li>
+        <li>
+          <a href="https://www.fyncom.com/terms-of-service">Terms of Use</a>
+        </li>
+        <li>
+          <a href="https://www.fyncom.com/sitemap">Sitemap</a>
+        </li>        <li>
+          <a href="https://www.fyncom.com/privacy-policy">Privacy Policy</a>
+        </li>
+      </ul>
+    </footer>
+  )
+}
+
+export default Footer
