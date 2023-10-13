a6e5ab9796cd73aa04073d564eff4441239c0f51
AEG-LaptopThinkpadP16s <52181546+adrianegraphene@users.noreply.github.com>
2023-10-13 10:21:24 -0700

    Setting up footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,63 +1,57 @@
 import React from "react"
+import "./footer.css" // Importing the CSS file
 
 const Footer = () => {
   return (
     <footer>
-      <div>
+      <div className="footer-header">
+        <h2>FynCom</h2>
         <p>
-          info@fyncom.com | We are located in Irvine, CA, focused in the USA &
-          operate globally.
+          <a href="mailto:info@fyncom.com">info@fyncom.com</a> | We are located
+          in Irvine, CA, focused in the USA & operate globally.
         </p>
+        <div className="footer-socials">
+          <a href="https://www.linkedin.com/company/fyncom">LinkedIn Pic</a>
+          <a href="https://twitter.com/Fyn_Com">Twitter Pic</a>
+          <a href="https://www.facebook.com/FynCom/">FB PIc</a>
+        </div>
       </div>
-      <ul className="footer-links">
-        <li>
-          <a href="https://www.fyncom.com">FynCom Website</a>
-        </li>
-        <li>
-          <a href="mailto:info@fyncom.com">info@fyncom.com</a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/company/fyncom">LinkedIn</a>
-        </li>
-        <li>
-          <a href="https://twitter.com/Fyn_Com">Twitter</a>
-        </li>
-        <li>
-          <a href="https://www.facebook.com/FynCom/">Facebook</a>
-        </li>
-        <li>
-          <a href="https://www.fyncom.com/use-cases">Product</a>
-        </li>
-        <li>
-          <a href="https://www.fyncom.com/use-cases">Reward Campaigns</a>
-        </li>
-        <li>
-          <a href="https://www.fyncom.com/about">About</a>
-        </li>
-        <li>
-          <a href="https://www.fyncom.com/pricing">Pricing</a>
-        </li>
-        {/*<li>*/}
-        {/*  <a href="https://www.fyncom.com/partners">Partners</a>*/}
-        {/*</li>*/}
-        <li>
-          <a href="https://www.fyncom.com/help-center">Help</a>
-        </li>
-        <li>
-          <a href="https://www.fyncom.com/blog">Blog</a>
-        </li>
-        <li>
-          <a href="https://www.fyncom.com/contact">Contact Us</a>
-        </li>
-        <li>
-          <a href="https://www.fyncom.com/terms-of-service">Terms of Use</a>
-        </li>
-        <li>
-          <a href="https://www.fyncom.com/sitemap">Sitemap</a>
-        </li>        <li>
-          <a href="https://www.fyncom.com/privacy-policy">Privacy Policy</a>
-        </li>
-      </ul>
+      <div className="footer-links-container">
+        <ul className="footer-links">
+          <li>
+            <a href="https://www.fyncom.com/use-cases">Product</a>
+          </li>
+          <li>
+            <a href="https://www.fyncom.com/use-cases">Reward Campaigns</a>
+          </li>
+          <li>
+            <a href="https://www.fyncom.com/about">About</a>
+          </li>
+          <li>
+            <a href="https://www.fyncom.com/pricing">Pricing</a>
+          </li>
+          <li>
+            <a href="https://www.fyncom.com/help-center">Help</a>
+          </li>
+          <li>
+            <a href="https://www.fyncom.com/blog">Blog</a>
+          </li>
+        </ul>
+        <ul className="footer-links">
+          <li>
+            <a href="https://www.fyncom.com/contact">Contact Us</a>
+          </li>
+          <li>
+            <a href="https://www.fyncom.com/terms-of-service">Terms of Use</a>
+          </li>
+          <li>
+            <a href="https://www.fyncom.com/sitemap">Sitemap</a>
+          </li>
+          <li>
+            <a href="https://www.fyncom.com/privacy-policy">Privacy Policy</a>
+          </li>
+        </ul>
+      </div>
     </footer>
   )
 }
--- src/components/header.js
+++ src/components/header.js
@@ -5,15 +5,29 @@ const Header = () => {
     <header>
       <img src="path_to_logo.png" alt="FynCom Logo" />
       <ul className="nav-links">
-        <li>Use Cases</li>
-        <li>About</li>
-        <li>Pricing</li>
-        <li>Blog</li>
-        <li>Help</li>
+        <li>
+          <a href="https://www.fyncom.com/use-cases">Use Cases</a>
+        </li>
+        <li>
+          <a href="https://www.fyncom.com/about">About</a>
+        </li>
+        <li>
+          <a href="https://www.fyncom.com/pricing">Pricing</a>
+        </li>
+        <li>
+          <a href="https://www.fyncom.com/blog">Blog</a>
+        </li>
+        <li>
+          <a href="https://www.fyncom.com/help-center">Help</a>
+        </li>
       </ul>
       <div>
-        <button>Business Login</button>
-        <button>User Login</button>
+        <a href="https://dashboard.fyncom.com/">
+          <button>Business Login</button>
+        </a>
+        <a href="https://app.fyncom.com/">
+          <button>User Login</button>
+        </a>
       </div>
     </header>
   )
