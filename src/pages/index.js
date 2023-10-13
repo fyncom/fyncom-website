874eecbc37659e526bd71ddb35f08f072716ff69
AEG-LaptopThinkpadP16s <52181546+adrianegraphene@users.noreply.github.com>
2023-10-13 10:21:24 -0700

    Setting up footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
 
+
 // 2. Main Component
 const BlockSpamEarnCash = () => {
   return (
