ad077380600afc66a24f42dee4d79bf7edd1052a
AEG-LaptopThinkpadP16s <52181546+adrianegraphene@users.noreply.github.com>
2023-10-12 20:44:19 -0700

    First setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,128 +1,74 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+// 1. Importing Components and Dependencies
+import React from "react"
+import Header from "../components/header"
+import Footer from "../components/footer"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-import * as styles from "../components/index.module.css"
+// 2. Main Component
+const BlockSpamEarnCash = () => {
+  return (
+    <div>
+      <Header /> {/* Using the Header component */}
+      {/* Main Content */}
+      <section>
+        <h1>Profit from Scam Blocking</h1>
+        <h2>Cash in effortlessly</h2>
+        <p>Emails, direct messages, calls - we do it all!</p>
 
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-  },
-  {
-    text: "Examples",
-    url: "https://github.com/gatsbyjs/gatsby/tree/master/examples",
-    description:
-      "A collection of websites ranging from very basic to complex/complete that illustrate how to accomplish specific tasks within your Gatsby sites.",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Learn how to add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-  },
-]
+        <section>
+          <h2>Are your customers getting phished?</h2>
+          <p>
+            Stop scams with refundable deposits. Our tech asks unknown senders
+            to risk losing money to your audience before they can reach them.
+            Learn more.
+          </p>
+        </section>
 
-const samplePageLinks = [
-  {
-    text: "Page 2",
-    url: "page-2",
-    badge: false,
-    description:
-      "A simple example of linking to another page within a Gatsby site",
-  },
-  { text: "TypeScript", url: "using-typescript" },
-  { text: "Server Side Rendering", url: "using-ssr" },
-  { text: "Deferred Static Generation", url: "using-dsg" },
-]
+        <section>
+          <h2>Too many scam DMs in chats?</h2>
+          <div>
+            <img src="path_to_discord_logo.png" alt="Discord Logo" />
+            <img src="path_to_tinder_logo.png" alt="Tinder Logo" />
+            <p>
+              Your community loves making connections, but not with scammers &
+              imposters. Help mods by adding FynCom's Direct Message tech into
+              your Discord, Telegram, or other chat-based community. Bonus -
+              your users make $$ for every blocked spam DM!{" "}
+              <a href="path_to_contact_page">Contact us</a>
+            </p>
+          </div>
+        </section>
 
-const moreLinks = [
-  { text: "Join us on Discord", url: "https://gatsby.dev/discord" },
-  {
-    text: "Documentation",
-    url: "https://gatsbyjs.com/docs/",
-  },
-  {
-    text: "Starters",
-    url: "https://gatsbyjs.com/starters/",
-  },
-  {
-    text: "Showcase",
-    url: "https://gatsbyjs.com/showcase/",
-  },
-  {
-    text: "Contributing",
-    url: "https://www.gatsbyjs.com/contributing/",
-  },
-  { text: "Issues", url: "https://github.com/gatsbyjs/gatsby/issues" },
-]
+        <section>
+          <h2>Why FynCom?</h2>
+          <p>
+            Our company is built around refundable deposits as a tool to create
+            trust between unknown parties in digital communications. Protect
+            yourself from unwanted communications & get the power to put a
+            monetary value to your time & data.{" "}
+            <a href="path_to_read_more">Read More</a>
+          </p>
+        </section>
 
-const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
+        <section>
+          <h2>A scam blocking tool for some...</h2>
+          <p>
+            ...and a rewards engine for all. FynCom creates trust in online
+            communications with its Refundable Deposits API. Any platform with
+            communication between users can give cash-back to users for blocking
+            scams. Bring a modern approach to cybersecurity that your customers
+            will love & your revenue teams will love more.
+          </p>
+        </section>
 
-const IndexPage = () => (
-  <Layout>
-    <div className={styles.textCenter}>
-      <StaticImage
-        src="../images/example.png"
-        loading="eager"
-        width={64}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt=""
-        style={{ marginBottom: `var(--space-3)` }}
-      />
-      <h1>
-        Welcome to <b>Gatsby!</b>
-      </h1>
-      <p className={styles.intro}>
-        <b>Example pages:</b>{" "}
-        {samplePageLinks.map((link, i) => (
-          <React.Fragment key={link.url}>
-            <Link to={link.url}>{link.text}</Link>
-            {i !== samplePageLinks.length - 1 && <> · </>}
-          </React.Fragment>
-        ))}
-        <br />
-        Edit <code>src/pages/index.js</code> to update this page.
-      </p>
+        <section>
+          <h2>Give the Gift of Cash-Back for Scam Blocking</h2>
+          <button>REQUEST A DEMO</button>
+        </section>
+      </section>
+      <Footer /> {/* Using the Footer component */}
     </div>
-    <ul className={styles.list}>
-      {links.map(link => (
-        <li key={link.url} className={styles.listItem}>
-          <a
-            className={styles.listItemLink}
-            href={`${link.url}${utmParameters}`}
-          >
-            {link.text} ↗
-          </a>
-          <p className={styles.listItemDescription}>{link.description}</p>
-        </li>
-      ))}
-    </ul>
-    {moreLinks.map((link, i) => (
-      <React.Fragment key={link.url}>
-        <a href={`${link.url}${utmParameters}`}>{link.text}</a>
-        {i !== moreLinks.length - 1 && <> · </>}
-      </React.Fragment>
-    ))}
-  </Layout>
-)
+  )
+}
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
-export const Head = () => <Seo title="Home" />
-
-export default IndexPage
+// 3. Exporting the Main Component
+export default BlockSpamEarnCash
