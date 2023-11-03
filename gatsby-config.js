60e4b290e52f7e12d4e848ff2e4cb2b580e2c5bc
AEG-Desktop <52181546+adrianegraphene@users.noreply.github.com>
2023-11-03 11:13:03 -0700

    Adding barlow font family and moving images into use-cases.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,28 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
+/** https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/ */
 
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
+/** @type {import('gatsby').GatsbyConfig} */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    // block spam calls/emails. My life goal is to make calls, emails, texts, DMs safe & enjoyable again.
+    title: `Sales Tools | CRM Rewards | Interactive Marketing  | FynCom | Merced CA`,
+    description: `Get more conversations by instantly rewarding prospects when they respond to emails or book meetings.
+      Break sales funnel bottlenecks with automated rewards. Get FynCom set up in minutes & enhance your tech stack.
+      Improve customer experience & max your chances of gaining & retaining customers.`,
+    author: `@adrianegraphene`,
+    siteUrl: `https://www.fyncom.com`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Barlow`,
+            variants: [`100`,`200`,`300`,`400`,`500`,'600',`700`,`800`,`900`]
+          },
+        ],
+      },
+    },
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -36,7 +44,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/FynCom_Logo_Circle-CanvasLargest.png`, // This path is relative to the root of the site.
       },
     },
   ],
--- src/pages/use-cases.js
+++ src/pages/use-cases.js
@@ -3,72 +3,73 @@ import Header from "../components/header"
 import Footer from "../components/footer"
 import mobileMarketing from "../images/marketing-mobile-survey.webp"
 // import mobileMarketing from "../images/marketing-mobile-survey.png"
+import phoneCalls from "../images/sales-phone-calls.webp"
+// import phoneCalls from "../images/sales-phone-calls.png"
+import closingDeals from "../images/close-deals-customers-engagement.webp"
+// import closingDeals from "../images/close-deals-customers-engagement.png"
 import "../components/use-cases.css"
 
 const UseCases = () => {
   return (
-    <div className="use-cases-container">
+    <div>
       <Header />
-      <h1>Drive the behavior you want</h1>
-      <p>Enhance your sales, marketing & customer feedback tools</p>
-      <p>Today's Attention Economy makes time spent with your brand increasingly valuable, yet people's attention span gets progressively worse!</p>
-      <p>Use FynCom's interactive rewards to grow the attention span of their target audiences, creating engagement that would otherwise be lost.</p>
+        <div className="use-cases-container">
+          <h1>Drive the behavior you want</h1>
+          <sub>Enhance your sales, marketing & customer feedback tools</sub>
+          <p>Enhance your sales, marketing & customer feedback tools</p>
+          <p>Today's Attention Economy makes time spent with your brand increasingly valuable, yet people's attention span gets progressively worse!</p>
+          <p>Use FynCom's interactive rewards to grow the attention span of their target audiences, creating engagement that would otherwise be lost.</p>
 
-      <div className="use-case-section">
-        <div className="use-case-image">
-          {/* Placeholder Image */}
-          <img src={mobileMarketing} alt="Understand your customers better" />
-        </div>
-        <div className="use-case-description">
-          <h2>01</h2>
-          <h3>Understand your customers better</h3>
-          <p>Treat each customer feedback survey as a reward campaign, reduce admin time, and generate trust with your contacts</p>
-          <a href="/use-cases/understand-customers" className="learn-more-btn">LEARN MORE</a>
-        </div>
-      </div>
+          <div className="use-case-section">
+            <div className="use-case-image">
+              <img src={mobileMarketing} alt="Understand your customers better" />
+            </div>
+            <div className="use-case-description">
+              <h3>Understand your customers better</h3>
+              <p>Treat each customer feedback survey as a reward campaign, reduce admin time, and generate trust with your contacts</p>
+              <a href="/use-cases/understand-customers" className="learn-more-btn">LEARN MORE</a>
+            </div>
+          </div>
 
-      <div className="use-case-section">
-        <div className="use-case-image">
-          {/* Placeholder Image */}
-          <img src="/path/to/image2.png" alt="Close more deals" />
-        </div>
-        <div className="use-case-description">
-          <h2>02</h2>
-          <h3>Close more deals</h3>
-          <p>Break large gift into strategic points on your customer journey to reduce your Cost Per Lead</p>
-          <a href="/use-cases/close-deals" className="learn-more-btn">LEARN MORE</a>
-        </div>
-      </div>
+          <div className="use-case-section">
+            <div className="use-case-description">
+              <h3>Close more deals</h3>
+              <p>Break large gift into strategic points on your customer journey to reduce your Cost Per Lead</p>
+              <a href="/use-cases/close-deals" className="learn-more-btn">LEARN MORE</a>
+            </div>
+            <div className="use-case-image">
+              <img src={phoneCalls} alt="Close more deals" />
+            </div>
+          </div>
 
-      <div className="use-case-section">
-        <div className="use-case-image">
-          {/* Placeholder Image */}
-          <img src="/path/to/image3.png" alt="Increase Customer Engagement" />
-        </div>
-        <div className="use-case-description">
-          <h2>03</h2>
-          <h3>Increase Customer Engagement</h3>
-          <p>Increase customer engagement and increase customer loyalty by offering incentivized rewards along the sales funnel to encourage your audience to learn more about your products and offerings</p>
-          <a href="/use-cases/customer-engagement" className="learn-more-btn">LEARN MORE</a>
-        </div>
-      </div>
+          <div className="use-case-section">
+            <div className="use-case-image">
+              <img src={closingDeals} alt="Increase Customer Engagement" />
+            </div>
+            <div className="use-case-description">
+              <h3>Increase Customer Engagement</h3>
+              <p>Increase customer engagement and increase customer loyalty by offering incentivized rewards along the sales funnel to encourage your audience to learn more about your products and offerings</p>
+              <a href="/use-cases/customer-engagement" className="learn-more-btn">LEARN MORE</a>
+            </div>
+          </div>
 
-      <div className="demo-form">
-        <h2>REQUEST A DEMO</h2>
-        <p>Get in contact with us!</p>
-        <form action="/submit-demo-request" method="post">
-          <input type="text" name="name" placeholder="Name" required />
-          <input type="email" name="email" placeholder="Email" required />
-          <input type="text" name="subject" placeholder="Subject" />
-          <textarea name="message" placeholder="Type your message here..." required></textarea>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
+          <div className="demo-form">
+            <h2>REQUEST A DEMO</h2>
+            <p>Get in contact with us!</p>
+            <form action="/submit-demo-request" method="post">
+              <input type="text" name="name" placeholder="Name" required />
+              <input type="email" name="email" placeholder="Email" required />
+              <input type="text" name="subject" placeholder="Subject" />
+              <textarea name="message" placeholder="Type your message here..." required></textarea>
+              <button type="submit">Submit</button>
+            </form>
+          </div>
 
-      <div className="fyncom-logo">
-        {/* Placeholder Logo */}
-        <img src="/path/to/fyncom-logo.png" alt="FynCom" />
-      </div>
+          <div className="fyncom-logo">
+            {/* Placeholder Logo */}
+            <img src="/path/to/fyncom-logo.png" alt="FynCom" />
+          </div>
+        </div>
       <Footer />
     </div>
   )
