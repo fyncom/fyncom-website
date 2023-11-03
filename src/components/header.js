3b72b392bc5d03014b2652d04d76715b296c117b
AEG-LaptopThinkpadP16s <52181546+adrianegraphene@users.noreply.github.com>
2023-11-02 22:26:42 -0700

    nicekly done header - note use of image size, rather than scaling to prevent the box effect

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,40 @@
 import React from "react"
+import fyncomLogo from "../images/FynCom_Logo_New-LARGEST.png"
+import "./header.css" // Importing the CSS file
+
 
 const Header = () => {
   return (
-    <header>
-      <img src="path_to_logo.png" alt="FynCom Logo" />
-      <ul className="nav-links">
-        <li>
-          <a href="https://www.fyncom.com/use-cases">Use Cases</a>
-        </li>
-        <li>
-          <a href="https://www.fyncom.com/about">About</a>
-        </li>
-        <li>
-          <a href="https://www.fyncom.com/pricing">Pricing</a>
-        </li>
-        <li>
-          <a href="https://www.fyncom.com/blog">Blog</a>
-        </li>
-        <li>
-          <a href="https://www.fyncom.com/help-center">Help</a>
-        </li>
-      </ul>
-      <div>
-        <a href="https://dashboard.fyncom.com/">
-          <button>Business Login</button>
+    <header className="header-top">
+      <div className="header-container">
+        <a href="https://www.fyncom.com">
+          <img src={fyncomLogo} alt="FynCom Logo" />
         </a>
-        <a href="https://app.fyncom.com/">
-          <button>User Login</button>
-        </a>
+        <ul className="nav-links">
+          <li>
+            <a href="https://www.fyncom.com/use-cases">Use Cases</a>
+          </li>
+          <li>
+            <a href="https://www.fyncom.com/about">About</a>
+          </li>
+          <li>
+            <a href="https://www.fyncom.com/pricing">Pricing</a>
+          </li>
+          <li>
+            <a href="https://www.fyncom.com/blog">Blog</a>
+          </li>
+          <li>
+            <a href="https://www.fyncom.com/help-center">Help</a>
+          </li>
+        </ul>
+        <div className="login-buttons">
+          <a href="https://dashboard.fyncom.com/">
+            <button className="business">Business Login</button>
+          </a>
+          <a href="https://app.fyncom.com/">
+            <button className="user">User Login</button>
+          </a>
+        </div>
       </div>
     </header>
   )
