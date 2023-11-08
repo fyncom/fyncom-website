7eca7aff54d64c61e5ffaaa680a777ab3925a3a6
AEG-Desktop <52181546+adrianegraphene@users.noreply.github.com>
2023-11-07 17:35:10 -0800

    getting markdown to show on help pages -1st step

--- src/pages/help-center.js
+++ src/pages/help-center.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import "../components/help-center.css";
+import MarkdownContent from "../components/MarkdownContent";
 
 const HelpCenter = () => {
+  const [selectedItemUrl, setSelectedItemUrl] = useState(null);
+
   const helpItems = [
     { title: "Overview of Rewards Tools", description: "Learn more about our services", icon: "info-circle" },
     { title: "Zapier Setup", description: "Automate Rewards in Zapier", icon: "zap" },
     { title: "Simple Email Setup", description: "Set up your forwarding email No API key required. 1 Rewards Campaign at a time", icon: "envelope" },
     { title: "Sendgrid API Key", description: "Add your API key to FynCom", icon: "key" },
-    { title: "Sendgrid Email Rewards", description: "Attach Rewards To Emails", icon: "mail-bulk" },
+    { title: "Sendgrid Email Rewards", description: "Attach Rewards To Emails", icon: "mail-bulk", url: "https://raw.githubusercontent.com/fyncom/fyncom-help/main/Attaching-FynCom-Rewards-To-Sendgrid-Emails.md" },
     { title: "Marketing & Basic Emails", description: "Set up your forwarding email Email API key required", icon: "chart-bar" },
     { title: "Sendgrid Drafts", description: "Prepare your Sendgrid draft for FynCom rewards", icon: "file-alt" },
     { title: "Account Balance", description: "Information about your balance", icon: "wallet" },
   ];
+
+  const handleClick = (url) => {
+    console.log('URL Loaded:', url); // This will print the URL to the console
+    setSelectedItemUrl(url);
+  };
 
   return (
     <div>
@@ -24,13 +32,17 @@ const HelpCenter = () => {
         <a href="/user-help-center">Not a business? Click here for our User Help Center.</a>
         <div className="help-items">
           {helpItems.map(item => (
-            <div key={item.title} className="help-item">
+            <div key={item.title}
+                 className="help-item"
+                 onClick={() => item.url && handleClick(item.url)}
+                 data-has-url={!!item.url}> {/* This will add the attribute if the item has a URL */}
               <div className={`icon ${item.icon}`}></div>
               <h2>{item.title}</h2>
               <p>{item.description}</p>
             </div>
           ))}
         </div>
+        {selectedItemUrl && <MarkdownContent url={selectedItemUrl} />}
       </div>
       <Footer />
     </div>
