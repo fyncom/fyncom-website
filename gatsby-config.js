7dd4dcf3b8291786819a43d0ee48106a841280bb
AEG-Desktop <52181546+adrianegraphene@users.noreply.github.com>
2023-11-08 00:54:01 -0800

    adding favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/FynCom_Logo_Circle-CanvasLargest.png`, // This path is relative to the root of the site.
+        icon: `src/images/fyncom-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
