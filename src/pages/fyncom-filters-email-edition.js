4322a114907719730e6a3756cbf59c72c799a9c6
AEG-Desktop <52181546+adrianegraphene@users.noreply.github.com>
2023-11-08 13:34:52 -0800

    adding edtiror config for easier intellij editing. unicorner page added

---
+++ src/pages/fyncom-filters-email-edition.js
@@ -0,0 +1,76 @@
+import React from "react"
+import Header from "../components/header"
+import Footer from "../components/footer"
+import mobileMarketing from "../images/illustrations/marketing-mobile-survey.webp"
+import phoneCalls from "../images/illustrations/sales-phone-calls.webp"
+import closingDeals from "../images/close-deals-customers-engagement.webp"
+import fyncomLogo from "../images/FynCom_Logo_New-LARGEST.png"
+import "../components/use-cases.css"
+
+const FyncomFiltersEmailEdition = () => {
+  return (
+    <div>
+      <Header />
+        <div className="use-cases-container">
+          <h1>Block Spam Emails</h1>
+          <sub>Save time. Only see unknown emails that are so important, senders are willing to lose money (to you) if you don't respond.</sub>
+          <br/>
+          <br/>
+          <p>Today's Attention Economy makes time spent with your brand increasingly valuable, yet people's attention span gets progressively worse!
+            Use FynCom's interactive rewards to grow the attention span of their target audiences, creating engagement that would otherwise be lost.</p>
+
+          <div className="use-case-section">
+            <div className="use-case-image">
+              <img src={mobileMarketing} alt="Understand your customers better" />
+            </div>
+            <div className="use-case-description">
+              <h3>Understand your customers better</h3>
+              <p>Treat each customer feedback survey as a reward campaign, reduce admin time, and generate trust with your contacts</p>
+              <a href="/understanding-customers-use-cases" className="learn-more-btn">LEARN MORE</a>
+            </div>
+          </div>
+
+          <div className="use-case-section">
+            <div className="use-case-description">
+              <h3>Close more deals</h3>
+              <p>Break large gift into strategic points on your customer journey to reduce your Cost Per Lead</p>
+              <a href="/sales-use-cases" className="learn-more-btn">LEARN MORE</a>
+            </div>
+            <div className="use-case-image">
+              <img src={phoneCalls} alt="Close more deals" />
+            </div>
+          </div>
+
+          <div className="use-case-section">
+            <div className="use-case-image">
+              <img src={closingDeals} alt="Increase Customer Engagement" />
+            </div>
+            <div className="use-case-description">
+              <h3>Increase Customer Engagement</h3>
+              <p>Increase customer engagement and increase customer loyalty by offering incentivized rewards along the sales funnel to encourage your audience to learn more about your products and offerings</p>
+              <a href="/marketing-use-cases" className="learn-more-btn">LEARN MORE</a>
+            </div>
+          </div>
+          <div className="demo-form">
+            <h2>REQUEST A DEMO</h2>
+            <p>Get in contact with us!</p>
+            <form action="/submit-demo-request" method="post">
+              <input type="text" name="name" placeholder="Name" required />
+              <input type="email" name="email" placeholder="Email" required />
+              <input type="text" name="subject" placeholder="Subject" />
+              <textarea name="message" placeholder="Type your message here..." required></textarea>
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+
+          {/*Div holds the dark/light - src for fallback*/}
+          <div className="fyncom-logo">
+            <img src={fyncomLogo} alt="FynCom Logo" />
+          </div>
+        </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default FyncomFiltersEmailEdition
---
+++ src/pages/unicorner.js
@@ -0,0 +1,64 @@
+import React from "react"
+import fyncomFilters from "../images/fyncom-filters.png"
+import fyncomLogo from "../images/FynCom_Logo_New-LARGEST.png"
+import googlePlayBadge from "../images/google-play-en.png"
+import appStoreBadge from "../images/apple-en.png"
+import "../components/unicorner.css"
+import { Link } from "gatsby"
+
+const Unicorner = () => {
+  return (
+      <div className="unicorner-container">
+        {/*todo see if you can update this to be fully left side*/}
+        {/*<div className="fyncom-logo">*/}
+        {/*    <img src={fyncomLogo} alt="FynCom Logo" />*/}
+        {/*</div>*/}
+        <h1>Spam Sucks!</h1>
+        <h2>Get instantly paid to block bad emails & calls with...</h2>
+        <div className="fyncom-filters-words">
+          <img src={fyncomFilters} alt="FynCom Filters" />
+        </div>
+
+        <div className="info-section">
+          <p>
+            Save time & let us pay you to ignore those scam emails & calls! Be
+            the first to know when our text / SMS blocking tech is ready by
+            signing up to one of our apps today!
+          </p>
+          <ul className="info-list">
+            <li>Calls</li>
+            <ul className="calls-list">
+              <li>
+                Android.{" "}<a href="https://play.google.com/store/apps/details?id=com.fyncom.robocash">Free.</a>
+                  <a href="https://play.google.com/store/apps/details?id=com.fyncom.robocash" className="store-badges">
+                      <img className="app-img" src={googlePlayBadge} alt="Get KarmaCall on Google Play" />
+                  </a>
+              </li>
+              <li>
+                iOS.{" "}<a href="https://apps.apple.com/us/app/karmacall/id1574524278">$1.99 with 1 month trial.</a>
+                  <a href="https://apps.apple.com/us/app/karmacall/id1574524278" className="store-badges">
+                      <img className="app-img" src={appStoreBadge} alt="Download KarmaCall on the App Store" />
+                  </a>
+              </li>
+            </ul>
+            <li>Emails</li>
+            <ul className="emails-list">
+              <li>Gmail.{" "}<Link to="/fyncom-filters-email-edition">Free for 1st 100 blocked emails per month.</Link>
+              </li>
+            </ul>
+          </ul>
+        </div>
+
+        <div className="footer-section">
+          <p>
+            Our FynCom Filters work in the background to save you time and get
+            you paid. To find out more about why we're doing this & why this is
+            the only real solution to spam & scams, see our story.
+          </p>
+        <Link to="/white-paper-original-scam-calls">our story</Link>
+        </div>
+      </div>
+  )
+}
+
+export default Unicorner
