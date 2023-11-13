7ffe8b5895e05cd421d0bd8a55f5d11544a68c90
AEG-Desktop <52181546+adrianegraphene@users.noreply.github.com>
2023-11-12 16:35:10 -0800

    improving about us for mobile

--- src/pages/about.js
+++ src/pages/about.js
@@ -33,13 +33,20 @@ const About = () => {
         <div className="story-text">
           <h3>Our Story</h3>
           <p>
-            FynCom's Rewards technology was invented to disrupt scam economics over phone calls with refundable cash deposits (<Link to="/white-paper-original-scam-calls">Read more here</Link>). As we spoke to businesses about our solution, we learned that getting responses to business communications is tough!
+            FynCom's Rewards technology was invented to disrupt scam economics over phone calls with refundable cash deposits {" "}
+            (<Link to="/white-paper-original-scam-calls">Read more here</Link>). As we spoke to businesses about our solution, we learned that getting
+            responses to business communications is tough!
           </p>
           <p>
-            So we modified our tech to create response generation technology that rewards prospects & customers when they respond to emails, calls, texts, surveys, and more. We saw incredible results from offering these immediate incentivized rewards, with tests showing a 200% increase in cold-call pick ups and 500% increase in email response rates.
+            So we modified our tech to create response generation technology that rewards prospects & customers when they respond to emails, calls, texts,
+            surveys, and more. We saw incredible results from offering these immediate incentivized rewards, with tests showing a 200% increase in cold-call
+            pick ups and 500% increase in email response rates.
           </p>
           <p>
-            That was enough to let us know there was value here. FynCom works over the top of any existing service and aims to be the Rewards layer of the internet. We are here to be the best way of transferring financial value in response to communications, hence the name "Financial(Fyn)Communications(Com)".
+            That was enough to let us know there was value here. FynCom works over the top of any existing service and aims to be the Rewards layer of the
+            internet, stopping spam & scams outright while allowing trust to grow again. We are here to be the best way of transferring financial value in
+            response to communications, so <p2 className="p2-blue">Fyn</p2><p2 className="text-wrapper-3-red">Com</p2> is short for
+            "<p2 className="p2-blue">Fin</p2>ancial <p2 className="text-wrapper-3-red">Com</p2>munications".
           </p>
         </div>
       </section>
@@ -49,27 +56,27 @@ const About = () => {
       <section className="values-section">
         <div className="value">
           <img src={innovation} alt="Innovation" />
-          <p>Innovation</p>
+          <h4>Innovation</h4>
           <p>We are always looking for new ways to push the boundaries and deliver value to our clients.</p>
         </div>
         <div className="value">
           <img src={collaboration} alt="Collaboration" />
-          <p>Collaboration</p>
+          <h4>Collaboration</h4>
           <p>We believe that great things can be achieved when we work together as a team.</p>
         </div>
         <div className="value">
           <img src={transparency} alt="Transparency" />
-          <p>Transparency</p>
+          <h4>Transparency</h4>
           <p>We are committed to open, honest communication and straightforward transparency.</p>
         </div>
         <div className="value">
           <img src={customerFocus} alt="Customer Focus" />
-          <p>Customer focus</p>
+          <h4>Customer focus</h4>
           <p>Our customers are at the heart of everything we do. We are dedicated to delivering the best possible experience for them. Focus is key.</p>
         </div>
         <div className="value">
           <img src={continuousImprovement} alt="Continuous Improvement" />
-          <p>Continuous improvement</p>
+          <h4>Continuous improvement</h4>
           <p>We believe that there is always room for growth and improvement. We are committed to continuous learning and development.</p>
         </div>
       </section>
