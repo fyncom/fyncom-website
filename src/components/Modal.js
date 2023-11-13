c141b3ce35ce465180ebbe7940c8d834f699f098
AEG-Desktop <52181546+adrianegraphene@users.noreply.github.com>
2023-11-13 13:43:26 -0800

    using modal components and light typo fix

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,6 +2,7 @@
 import React from "react";
 import "../components/contact.css";
 import "../components/blocked-email.css";
+import nanoQrCode from "../images/DepositNanoQRCode.jpg";
 // should import all into modal.css later
 
 export const SuccessModal = ({ isOpen, message, onClose }) => {
@@ -29,3 +30,21 @@ export const FailureModal = ({ isOpen, message, onClose }) => {
     </div>
   );
 };
+
+export const MakeADepositModal = ({ onClose }) => {
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <span className="close" onClick={onClose}>&times;</span>
+        <h2>Got nano?</h2>
+        <p>Use "nano"-transactions!</p>
+        <p>Deposit 0.1 nano to</p>
+        <p className="nano-address">nano_1bf3r8pqfsutekxunazj895an8h84ai3ao1ftqyejqiul65p3xsb9k99kc1</p>
+        <div className="qr-code-container">
+          <img src={nanoQrCode} alt="QR Code of the nano address you should send your deposit to" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
