62772c43d9f0bd2440e11772d6069b3e3866bf95
AEG-LaptopThinkpadP16s <52181546+adrianegraphene@users.noreply.github.com>
2023-11-14 20:46:09 -0800

    blog wrapper working now

--- src/components/Markdown-Wrapper.js
+++ src/components/Markdown-Wrapper.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import "../components/markdown.css"
+import Header from "./header";
+import Footer from "./footer";
+import Seo from "./seo"
 
-export const Wrapper = ({ children }) => (
-  <div className="gatsby-focus-wrapper">{children}</div>
+export const Wrapper = ({ children, title, description }) => (
+  <div>
+    <Seo
+      title={title}
+      description={description}
+    />
+    <Header />
+    <div className="gatsby-focus-wrapper">{children}</div>
+    <Footer />
+  </div>
 );
 
 export default ({ children }) => <Wrapper>{children}</Wrapper>;
