1c7b1d4ae9391712eb041c003f34b0622917a2ce
AEG-Desktop <52181546+adrianegraphene@users.noreply.github.com>
2023-11-16 05:59:11 -0800

    fixing footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,18 +1,52 @@
 import React from "react"
 import "./footer.css"
-import fbLogo from "../images/logos/facebook-logo.webp"
-import linkedInlogo from "../images/logos/linkedin-logo.webp"
-import xLogo from "../images/logos/twitter-logo-change-x-elon-musk-designboom-500.webp"
-import fyncomLogoWhite from "../images/fyncom-logo-white.png"
-import { Link } from "gatsby";
+import {graphql, Link, useStaticQuery} from "gatsby";
+import {GatsbyImage, getImage} from "gatsby-plugin-image";
+import Img from "gatsby-image";
+
 
 const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      fyncomLogoWhite: file(relativePath: { eq: "fyncom-logo-white.png" }) {
+        childImageSharp {
+          gatsbyImageData(width: 80, layout: CONSTRAINED, placeholder: BLURRED)
+        }
+      }
+      linkedInlogo: file(relativePath: { eq: "logos/linkedin-logo.webp" }) {
+        childImageSharp {
+          fixed(width: 24) {
+            ...GatsbyImageSharpFixed_withWebp_noBase64
+          }
+        }
+      }
+     fbLogo: file(relativePath: { eq: "logos/facebook-logo.webp" }) {
+      childImageSharp {
+        fixed(width: 24) {
+          ...GatsbyImageSharpFixed_withWebp_noBase64
+        }
+      }
+    }
+    xLogo: file(relativePath: { eq: "logos/x-logo.png" }) {
+      childImageSharp {
+        fixed(width: 24) {
+          ...GatsbyImageSharpFixed_withWebp_noBase64
+        }
+      }
+    }
+  }
+  `);
+  const fyncomLogoWhite = getImage(data.fyncomLogoWhite.childImageSharp.gatsbyImageData);
+  const linkedInlogo = data.linkedInlogo.childImageSharp.fixed;
+  const fbLogo = data.fbLogo.childImageSharp.fixed;
+  const xLogo = data.xLogo.childImageSharp.fixed;
+
   return (
     <div>
     <footer>
       <div className="footer-header">
         <a href="https://www.linkedin.com/company/fyncom">
-          <img src={fyncomLogoWhite} alt="FynCom Logo" />
+          <GatsbyImage image={fyncomLogoWhite} alt="FynCom Logo and LinkedIn Profile" />
         </a>
         <div className="info-container">
           <p>
@@ -27,19 +61,16 @@ const Footer = () => {
       <div className="footer-links-container">
         <div className="footer-socials">
           <a href="https://www.linkedin.com/company/fyncom">
-            <img src={linkedInlogo} alt="FynCom's Linkedin Page" />
+            <Img fixed={linkedInlogo} alt="FynCom's Linkedin Page" />
           </a>
           <a href="https://twitter.com/Fyn_Com">
-            <img
-              src={xLogo}
-              alt="FynCom's X (formally known as Twitter) logo"
-            />
+            <Img fixed={xLogo} alt="FynCom's X (formally known as Twitter) logo" />
           </a>
           <a href="https://www.facebook.com/FynCom/">
-            <img src={fbLogo} alt="FynCom's Facebook page" />
+            <Img fixed={fbLogo} alt="FynCom's Facebook page" />
           </a>
         </div>
-        <ul className="footer-links">
+        <ul className="footer-links left">
           {/*todo set this up so that only 1 is visble*/}
           <li>
             <Link to="/use-cases">Product</Link>
@@ -60,7 +91,7 @@ const Footer = () => {
             <Link to="/blog">Blog</Link>
           </li>
         </ul>
-        <ul className="footer-links">
+        <ul className="footer-links right">
           <li>
             <Link to="/contact">Contact Us</Link>
           </li>
