4c8a33ff8b696af66525385d90f18399c450a609
AEG-LaptopThinkpadP16s <52181546+adrianegraphene@users.noreply.github.com>
2023-11-17 15:36:09 -0800

    updating white paper visuals / text

--- src/pages/white-paper-original-scam-calls.js
+++ src/pages/white-paper-original-scam-calls.js
@@ -5,6 +5,8 @@ import "../components/white-paper.css"
 import whitePaper from "../../static/pdfs/fyncom-Original-White-Paper-For-KarmaCall-Update.pdf"
 import Seo from "../components/seo"
 import PdfContent from "../components/PdfContent"
+import { Link } from 'gatsby';
+
 
 const WhitePaperOriginalScamCalls = () => {
   return (
@@ -15,12 +17,19 @@ const WhitePaperOriginalScamCalls = () => {
       />
       <Header />
       <div className="content-container">
-        <h2>Why Do Spam Calls Still Exist?</h2>
-        <p>...and how can I stop scams, but let good callers reach me?</p>
+        <h2 className={"centered"}>Why Do Spam Calls Still Exist?</h2>
+        <p className={"centered"}>...and how can I stop scams, but let good callers reach me?</p>
+        <p>That's the thought that started me on a journey of exploring an emerging market that blends communications & currency to create trust between
+          strangers with shared interests. Here's the paper I wrote to record my thoughts - it later became{" "}
+          <a href="https://patents.google.com/patent/US11310368B2">my 1st patent</a>,{" "}
+          <a href="https://www.karmacall.com/">app</a>,{" "} and is the basis for how FynCom came to be.
+          Thanks for reading! <br/>
+          <i>- Adrian</i>
+        </p>
         <PdfContent file={whitePaper} />
         <p>
           Thanks for reading our White Paper on stopping spam across the world,
-          starting with scam calls!
+          starting with scam calls! <Link to={"/"}>Try out the  apps today </Link>to get started!
         </p>
       </div>
       <Footer />
