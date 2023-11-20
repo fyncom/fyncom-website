1c3d61dc132e7b64605998294bb50299ce431895
AEG-Desktop <52181546+adrianegraphene@users.noreply.github.com>
2023-11-20 01:58:14 -0800

    first updates

--- src/pages/rewards.js
+++ src/pages/rewards.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { useLocation } from "@reach/router";
-import {GiftCardSentToEmail, GiftCardNotEnoughBalance, GiftCardTxIdNotFound} from "../components/Modal"
+import { useLocation, Router } from "@reach/router";
+import { GiftCardSentToEmail, GiftCardNotEnoughBalance, GiftCardTxIdNotFound } from "../components/Modal"
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 const RewardsPage = () => {
-  const [isSuccessModalOpen, setSuccessModalOpen] = useState(false)
-  const [isFailureModalOpen, setFailureModalOpen] = useState(false)
-  const [isLowBalanceModalOpen, setLowBalanceModalOpen] = useState(false)
+  // Modal states should be individual and independent, not toggles
+  const [isSuccessModalOpen, setSuccessModalOpen] = useState(false);
+  const [isFailureModalOpen, setFailureModalOpen] = useState(false);
+  const [isLowBalanceModalOpen, setLowBalanceModalOpen] = useState(false);
   const [result, setResult] = useState(null);
+
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
 
@@ -14,23 +18,21 @@ const RewardsPage = () => {
     const txId = queryParams.get('txId');
     const value = queryParams.get('value');
     const type = queryParams.get('type');
-    console.log(`URL txId is ${txId}, URL value is ${value}, URL type is ${type}`);
 
     if (txId) {
       processReward(txId, value, type).then(data => {
         setResult(data);
-        // Handle opening of modal based on result here
+        setSuccessModalOpen(true); // Open the success modal on data receipt
       }).catch(error => {
         console.error('ERROR', error);
-        // Handle opening of error modal here
-        // consider adding general error modal here....
-
+        // Handle error based on the custom error message property or status
+        setFailureModalOpen(true); // If you need a general failure modal
       });
     }
   }, [queryParams]);
 
   const processReward = (txId, value, type) => {
-    let newUrl = `${process.env.GATSBY_API_URL}app/rewards/gcow/easy`
+    let url = `${process.env.GATSBY_API_URL}/v2/app/rewards/gcow/easy`;
     const headers = new Headers({
       'Content-Type': 'application/json',
       'Accept': 'application/json',
@@ -46,33 +48,45 @@ const RewardsPage = () => {
       })
     })
       .then(res => {
+        // Modal logic should not be inside fetch, handle modals outside
+        if(res.status === 400) {
+          setFailureModalOpen(true);
+        } else if(res.status === 422) {
+          setLowBalanceModalOpen(true);
+        }
         if (!res.ok) {
-          if (res.status === 400) {
-            {isFailureModalOpen && <GiftCardTxIdNotFound onClose={toggleModal} />}
-          } else if (res.status === 422) {
-            {isLowBalanceModalOpen && <GiftCardNotEnoughBalance onClose={toggleModal} />}
-          }
           throw new Error(`HTTP error! status: ${res.status}`);
         }
         return res.json();
       })
       .then(data => {
-        {isSuccessModalOpen && <GiftCardSentToEmail organizationName={data.organizationName} onClose={toggleModal} />}
+        setSuccessModalOpen(true); // On success
         console.log("Data received:", data);
         return data;
       })
       .catch(error => {
-        // Your error handling logic goes here
         console.error("Fetch error:", error);
+        setFailureModalOpen(true); // On fetch error
         throw error;
       });
   };
 
+  // Close modals by setting their respective states to false
+  const handleCloseModal = () => {
+    setSuccessModalOpen(false);
+    setFailureModalOpen(false);
+    setLowBalanceModalOpen(false);
+  };
+
   return (
     <div>
+      <Header/>
       <h1>Rewards Page</h1>
       {/* Render your page contents and modals based on state here */}
-      {result && <Modal result={result} />}
+      <GiftCardSentToEmail isOpen={isSuccessModalOpen} organizationName={result?.organizationName} onClose={handleCloseModal} urlRedirect={result?.preferences?.urlRedirect} />
+      <GiftCardTxIdNotFound isOpen={isFailureModalOpen} onClose={handleCloseModal} />
+      <GiftCardNotEnoughBalance isOpen={isLowBalanceModalOpen} onClose={handleCloseModal} />
+      <Footer/>
     </div>
   );
 };
