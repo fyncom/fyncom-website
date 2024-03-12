b0f637d7891db3d022f68644125072afa0a377c6
AEG-P16sG2 <52181546+adrianegraphene@users.noreply.github.com>
2024-03-12 12:38:11 -0700

    email settings update

--- src/pages/user-help-center/email-filters-and-rewards.js
+++ src/pages/user-help-center/email-filters-and-rewards.js
@@ -15,40 +15,27 @@ const EmailFiltersAndRewards = () => {
 
   return (
     <div className="email-filters-rewards-container">
-      <Seo
-        title="FynCom Filters: Email Edition"
-        description="setup your meail filters here."
-      />
+      <Seo title="FynCom Filters: Email Edition" description="setup your meail filters here." />
       <Header />
-      <Breadcrumbs
-        link={"/user-help-center"}
-        title="FynCom Filters: Email Edition"
-      />
+      <Breadcrumbs link={"/user-help-center"} title="FynCom Filters: Email Edition" />
 
       <main className={"filter-custom-page"}>
         <section className="setup-section">
           <h1>FynCom Filters: Email Edition</h1>
-          <p>
-            Set up your filter, manage settings, and more. Towards the bottom,
-            you can see our development roadmap.
-          </p>
+          <p>Set up your filter, manage settings, and more. Towards the bottom, you can see our development roadmap.</p>
           <div className="setup-instructions">
             <h2>Setup</h2>
-            <p>
+            {/* <p>
               <i>
                 Block less than 100 emails a month? Try the free version by
                 following the steps below.
               </i>
-            </p>
+            </p> */}
             <ol>
               <li>
-                Create an account on{" "}
-                <a href="https://app.fyncom.com">app.fyncom.com</a> using "Sign
-                in with Google".
+                Create an account on <a href="https://app.fyncom.com">app.fyncom.com</a> using "Sign in with Google".
                 <ul>
-                  <li>
-                    Check your email inbox to click the email verification link.
-                  </li>
+                  <li>Check your email inbox to click the email verification link.</li>
                 </ul>
               </li>
               <li>
@@ -56,10 +43,7 @@ const EmailFiltersAndRewards = () => {
                 <ul>
                   <li>Select "Email Filters".</li>
                   <li>
-                    Select "Sign in with Google" from the{" "}
-                    <a href="https://app.fyncom.com/settings">
-                      "Best Experience"
-                    </a>
+                    Select "Sign in with Google" from the <a href="https://app.fyncom.com/settings">"Best Experience"</a>
                     {""} box.
                   </li>
                   <li>Approve the permission requests.</li>
@@ -79,16 +63,10 @@ const EmailFiltersAndRewards = () => {
                   <li>
                     <a href="https://github.com/fyncom/whitelist/blob/main/domains.json">
                       {" "}
-                      The global whitelist is publicly viewable and updated in
-                      realt-time.
+                      The global whitelist is publicly viewable and updated in realt-time.
                     </a>{" "}
-                    Domains like .edu and .gov are whitelisted by default. A
-                    number of other domains and email senders are whitelisted
-                    here.{" "}
-                    <a href="https://github.com/fyncom/whitelist">
-                      Guidelines are here
-                    </a>
-                    .
+                    Domains like .edu and .gov are whitelisted by default. A number of other domains and email senders are whitelisted here.{" "}
+                    <a href="https://github.com/fyncom/whitelist">Guidelines are here</a>.
                   </li>
                 </ul>
               </li>
@@ -96,13 +74,8 @@ const EmailFiltersAndRewards = () => {
                 Personal Whitelist
                 <ul>
                   <li>
-                    To move emails into your personal whitelist, you can drag an
-                    email out of the FynFiltered and into the FynWhiteList email
-                    label.{" "}
-                    <a href="https://youtu.be/XgVJ7qrwlHk?t=189">
-                      See Timestamp here
-                    </a>
-                    .
+                    To move emails into your personal whitelist, you can drag an email out of the FynFiltered and into the FynWhiteList email label.{" "}
+                    <a href="https://youtu.be/XgVJ7qrwlHk?t=189">See Timestamp here</a>.
                   </li>
                 </ul>
               </li>
