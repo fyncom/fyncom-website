a37346c3fa278b879a4cfe4a700b56cb45f0046c
AEG-Desktop <52181546+adrianegraphene@users.noreply.github.com>
2025-05-31 14:05:39 -0700

    better contact us

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,7 +11,8 @@ import { GatsbyImage } from "gatsby-plugin-image"
 import { useCombinedQuery } from "../components/useCombinedQuery"
 
 const Contact = () => {
-  const { fyncomFiltersWords, fyncomFiltersWordsDark } = useCombinedQuery()
+  const { fyncomFiltersWords, fyncomFiltersWordsDark, oneMillionCups, disruptionBanking, evonexus, foundersNetwork, gasseeConsulting, nanoFoundation } =
+    useCombinedQuery()
   const [isSuccessModalOpen, setSuccessModalOpen] = useState(false)
   const [isFailureModalOpen, setFailureModalOpen] = useState(false)
   const [formData, setFormData] = useState({ name: "", email: "", message: "" })
@@ -68,157 +69,125 @@ const Contact = () => {
 
   return (
     <div>
-      <Seo
-        title="Contact Us"
-        description="Send a contact request to Team FynCom and we'll get back to you shortly"
-      />
+      <Seo title="Contact Us" description="Send a contact request to Team FynCom and we'll get back to you shortly" />
       <Header />
       <main className="contact-container">
-        <h1>Contact Us</h1>
-        <p>
-          If you have any questions or inquiries, feel free to reach out to us.
-        </p>
-        <form className="contact-form" onSubmit={handleSubmit}>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            // required
-          />
-          <label htmlFor="message">Message:</label>
-          <textarea
-            id="message"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            required
-          />
-          <button type="submit" className="submit-btn">
-            Send Message
-          </button>
-        </form>
+        <div className="contact-layout">
+          <div className="contact-left">
+            <h1>Contact our experts</h1>
+            <p className="contact-description">FynCom simplifies the response generation process for businesses and individuals.</p>
+            <p className="contact-description">Drive real engagement in a fraction of the time, with our curated, high-integrity incentive portfolios.</p>
+            <p className="contact-reach-out">
+              Reach out to us directly at <Link to="mailto:support@fyncom.com">support@fyncom.com</Link>
+            </p>
 
-        <h2>What is FynCom?</h2>
-        <p>
-          FynCom helps you stop spam and scams, while making money for yourself
-          or your organization.
-        </p>
-        <p>
-          We build "money layers" on top of communications platforms / protocols
-          & enforce escrow between unknown parties while rewarding people who
-          block spam.
-        </p>
-        <p>
-          We're a tech company that help people trust emails, calls, direct
-          messages and any sort of inbound communication.
-        </p>
-        <p>
-          We do this by integrating refundable transactions into the tools we
-          use everyday.
-        </p>
+            <div className="contact-supporters">
+              <div className="supporters-logos">
+                <div className="supporter-logo">
+                  <img src={evonexus} alt="EvoNexus" />
+                </div>
+                <div className="supporter-logo">
+                  <GatsbyImage image={oneMillionCups} alt="1 Million Cups" />
+                </div>
+                <div className="supporter-logo">
+                  <GatsbyImage image={disruptionBanking} alt="Disruption Banking" />
+                </div>
+                <div className="supporter-logo">
+                  <GatsbyImage image={foundersNetwork} alt="Founders Network" />
+                </div>
+                <div className="supporter-logo">
+                  <GatsbyImage image={gasseeConsulting} alt="Gassée Consulting" />
+                </div>
+                <div className="supporter-logo">
+                  <GatsbyImage image={nanoFoundation} alt="Nano Foundation" />
+                </div>
+              </div>
+            </div>
+          </div>
 
-        <i>
+          <div className="contact-right">
+            <form className="contact-form" onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="name">First Name*</label>
+                <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Business Email*</label>
+                <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="message">How can we help?</label>
+                <textarea id="message" name="message" value={formData.message} onChange={handleChange} rows="4" required />
+              </div>
+              <button type="submit" className="submit-btn">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+
+        <div className="contact-additional-content">
+          <h2>What is FynCom?</h2>
+          <p>FynCom helps you stop spam and scams, while making money for yourself or your organization.</p>
           <p>
-            Sometimes our tools like consumer products as shown towards the page
-            bottom...
+            We build "money layers" on top of communications platforms / protocols & enforce escrow between unknown parties while rewarding people who block
+            spam, respond to messages, or engage with the sales funnels of your organization .
           </p>
-        </i>
+          <p>We're a tech company that help people trust emails, calls, direct messages and any sort of inbound communication.</p>
+          <p>We do this by integrating refundable transactions into the tools we use everyday.</p>
 
-        <p>
-          But, most of our focus is on helping businesses create trust in their
-          communications. Whether that's in helping prevent business imposters
-          or government imposters from reducing your brand affinity or by giving
-          revenue operations teams the tools to enhance sales & marketing
-          efforts by rewarding people to respond / engage with outbound messages
-        </p>
+          <i>
+            <p>Sometimes our tools like consumer products as shown towards the page bottom...</p>
+          </i>
 
-        <i>
-          <p className="centered">Examples of our consumer tools</p>
-        </i>
-        <h1 className="centered">Spam Sucks!</h1>
-        <h2 className="centered">
-          Get instantly paid to block bad emails & calls with...
-        </h2>
-        <Link to="/fyncom-filters-email-edition">
-          <GatsbyImage image={filterLogo} alt="FynCom Filters" />
-        </Link>
-        <div className="info-section">
           <p>
-            Save time & let us pay you to ignore those scam emails & calls! Be
-            the first to know when our text / SMS blocking tech is ready by
-            signing up to one of our apps today!
+            But, most of our focus is on helping businesses create trust in their communications. Whether that's in helping prevent business imposters or
+            government imposters from reducing your brand affinity or by giving revenue operations teams the tools to enhance sales & marketing efforts by
+            rewarding people to respond / engage with outbound messages
           </p>
-          <ul className="info-list">
-            <li>Calls</li>
-            <ul className="calls-list">
-              <li>
-                Android.{" "}
-                <a href="https://play.google.com/store/apps/details?id=com.fyncom.robocash">
-                  Free.
-                </a>
-                <a
-                  href="https://play.google.com/store/apps/details?id=com.fyncom.robocash"
-                  className="store-badges"
-                >
-                  <img
-                    className="app-img"
-                    src={googlePlayBadge}
-                    alt="Get KarmaCall on Google Play"
-                  />
-                </a>
-              </li>
-              <li>
-                iOS.{" "}
-                <a href="https://apps.apple.com/us/app/karmacall/id1574524278">
-                  $1.99 with 1 month trial.
-                </a>
-                <a
-                  href="https://apps.apple.com/us/app/karmacall/id1574524278"
-                  className="store-badges"
-                >
-                  <img
-                    className="app-img"
-                    src={appStoreBadge}
-                    alt="Download KarmaCall on the App Store"
-                  />
-                </a>
-              </li>
+
+          <i>
+            <p className="centered">Examples of our consumer tools</p>
+          </i>
+          <h1 className="centered">Spam Sucks!</h1>
+          <h2 className="centered">Get instantly paid to block bad emails & calls with...</h2>
+          <Link to="/fyncom-filters-email-edition">
+            <GatsbyImage image={filterLogo} alt="FynCom Filters" />
+          </Link>
+          <div className="info-section">
+            <p>
+              Save time & let us pay you to ignore those scam emails & calls! Be the first to know when our text / SMS blocking tech is ready by signing up to
+              one of our apps today!
+            </p>
+            <ul className="info-list">
+              <li>Calls</li>
+              <ul className="calls-list">
+                <li>
+                  Android. <a href="https://play.google.com/store/apps/details?id=com.fyncom.robocash">Free.</a>
+                  <a href="https://play.google.com/store/apps/details?id=com.fyncom.robocash" className="store-badges">
+                    <img className="app-img" src={googlePlayBadge} alt="Get KarmaCall on Google Play" />
+                  </a>
+                </li>
+                <li>
+                  iOS. <a href="https://apps.apple.com/us/app/karmacall/id1574524278">$1.99 with 1 month trial.</a>
+                  <a href="https://apps.apple.com/us/app/karmacall/id1574524278" className="store-badges">
+                    <img className="app-img" src={appStoreBadge} alt="Download KarmaCall on the App Store" />
+                  </a>
+                </li>
+              </ul>
+              <li>Emails</li>
+              <ul className="emails-list">
+                <li>
+                  Gmail. <Link to="/fyncom-filters-email-edition">Free for 1st 100 blocked emails per month.</Link>
+                </li>
+              </ul>
             </ul>
-            <li>Emails</li>
-            <ul className="emails-list">
-              <li>
-                Gmail.{" "}
-                <Link to="/fyncom-filters-email-edition">
-                  Free for 1st 100 blocked emails per month.
-                </Link>
-              </li>
-            </ul>
-          </ul>
+          </div>
         </div>
       </main>
       <Footer />
-      <SuccessModal
-        isOpen={isSuccessModalOpen}
-        message={modalMessage}
-        onClose={() => setSuccessModalOpen(false)}
-      />
-      <FailureModal
-        isOpen={isFailureModalOpen}
-        message={modalMessage}
-        onClose={() => setFailureModalOpen(false)}
-      />
+      <SuccessModal isOpen={isSuccessModalOpen} message={modalMessage} onClose={() => setSuccessModalOpen(false)} />
+      <FailureModal isOpen={isFailureModalOpen} message={modalMessage} onClose={() => setFailureModalOpen(false)} />
     </div>
   )
 }
